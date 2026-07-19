1171569eee82585f35f631c2673ef9dbb65b359f
tbhglobal <ted@tbhglobal.com>
2026-07-19 09:47:26 +0930

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,3 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { output: 'export', trailingSlash: true, images: { unoptimized: true } };
+const nextConfig = { trailingSlash: true, images: { unoptimized: true } };
 export default nextConfig;
