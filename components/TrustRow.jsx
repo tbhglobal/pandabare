11cc2e8e34239b53431b93f397aba8e8c9ab2660
tbhglobal <ted@tbhglobal.com>
2026-07-21 21:33:58 +0930

    Add files via upload

--- components/TrustRow.jsx
+++ components/TrustRow.jsx
@@ -4,7 +4,7 @@ const items = [
   { h: "Fast shipping", p: "Australia wide", d: "M3 7h11v8H3z M14 10h4l3 3v2h-7z M7 17a2 2 0 104 0 M17 17a2 2 0 104 0" },
   { h: "30 day returns", p: "No stress", d: "M4 12a8 8 0 108-8 M4 12H1 M4 12l3-3 M4 12l3 3" },
   { h: "Secure payments", p: "100% safe checkout", d: "M6 11V8a6 6 0 0112 0v3 M5 11h14v9H5z M12 15v2" },
-  { h: "1% for the planet", p: "We give back", d: "M12 21s-7-4.5-7-10a4 4 0 017-2.6A4 4 0 0119 11c0 5.5-7 10-7 10z" },
+  { h: "Organic bamboo", p: "Naturally soft", d: "M5 21c0-9 5-15 15-16 0 10-5 16-15 16z M8 18c3-5 6-8 10-9" },
 ];
 
 export default function TrustRow({ showHeading = true }) {
